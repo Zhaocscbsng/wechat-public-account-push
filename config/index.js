b11e20b77a956824b16166602968cc2e432b6cda
Zhaocscbsng <89082230+Zhaocscbsng@users.noreply.github.com>
2022-09-21 11:03:16 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ export const config = {
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "f1b33d85a056cc4c3b53f1f972dfd680",
+  APP_SECRET: "0babba72023f828efbaa7d26d3813dcf",
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
